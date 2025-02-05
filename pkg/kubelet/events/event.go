vsreedharv/kubernetes

adding event for restart container
Signed-off-by: Ritikaa96 <ritika@india.nec.com>

pkg/kubelet/events/event.go
@@ -20,6 +20,7 @@ package events
 const (
 	CreatedContainer        = "Created"
 	StartedContainer        = "Started"
+	RestartedContainer      = "ContainerExited"
 	FailedToCreateContainer = "Failed"
 	FailedToStartContainer  = "Failed"
 	KillingContainer        = "Killing"

adivyoseph/ai_kubelet

pkg/kubelet/events/event.go
@@ -20,6 +20,7 @@ package events
 const (
 	CreatedContainer        = "Created"
 	StartedContainer        = "Started"
+	RestartedContainer      = "Restarted"
 	FailedToCreateContainer = "Failed"
 	FailedToStartContainer  = "Failed"
 	KillingContainer        = "Killing"