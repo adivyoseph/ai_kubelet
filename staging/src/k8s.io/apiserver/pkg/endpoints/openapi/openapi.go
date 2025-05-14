2066c48b0a8ca3f1ce92e1b1c4cae857b3a41be1
Joe Betz <jpbetz@google.com>
2025-05-14 13:18:54 -0400

    Remove name mungaing from namer

--- staging/src/k8s.io/apiserver/pkg/endpoints/openapi/openapi.go
+++ staging/src/k8s.io/apiserver/pkg/endpoints/openapi/openapi.go
@@ -29,6 +29,7 @@ import (
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/klog/v2"
 	"k8s.io/kube-openapi/pkg/util"
 	"k8s.io/kube-openapi/pkg/validation/spec"
 )
@@ -150,7 +151,12 @@ func NewDefinitionNamer(schemes ...*runtime.Scheme) *DefinitionNamer {
 		for gvk, rtype := range s.AllKnownTypes() {
 			newGVK := gvkConvert(gvk)
 			exists := false
-			for _, existingGVK := range ret.typeGroupVersionKinds[typeName(rtype)] {
+			name, err := s.ToOpenAPIDefinitionName(gvk)
+			if err != nil {
+				klog.Fatalf("failed to get OpenAPI definition name for %v: %v", gvk, err)
+				continue
+			}
+			for _, existingGVK := range ret.typeGroupVersionKinds[name] {
 				if newGVK == existingGVK {
 					exists = true
 					break
@@ -170,9 +176,9 @@ func NewDefinitionNamer(schemes ...*runtime.Scheme) *DefinitionNamer {
 // GetDefinitionName returns the name and tags for a given definition
 func (d *DefinitionNamer) GetDefinitionName(name string) (string, spec.Extensions) {
 	if groupVersionKinds, ok := d.typeGroupVersionKinds[name]; ok {
-		return util.ToRESTFriendlyName(name), spec.Extensions{
+		return name, spec.Extensions{
 			extensionGVK: groupVersionKinds.JSON(),
 		}
 	}
-	return util.ToRESTFriendlyName(name), nil
+	return name, nil
 }
