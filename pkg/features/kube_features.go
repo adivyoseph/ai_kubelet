5b1660604afb9133eefca2c3609e3e21c3bf69d2
Filip Křepinský <atiratree@gmail.com>
2025-07-20 23:06:13 +0200

    promote DeploymentReplicaSetTerminatingReplicas to Beta

--- pkg/features/kube_features.go
+++ pkg/features/kube_features.go
@@ -1167,6 +1167,7 @@ var defaultVersionedKubernetesFeatureGates = map[featuregate.Feature]featuregate
 
 	DeploymentReplicaSetTerminatingReplicas: {
 		{Version: version.MustParse("1.33"), Default: false, PreRelease: featuregate.Alpha},
+		{Version: version.MustParse("1.34"), Default: true, PreRelease: featuregate.Beta},
 	},
 
 	DevicePluginCDIDevices: {
