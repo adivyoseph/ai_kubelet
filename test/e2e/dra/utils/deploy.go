9e62d1c50ca7a62233b121f27b77c67a7a3ce7e9
Patrick Ohly <patrick.ohly@intel.com>
2025-07-23 16:36:57 +0200

    Revert "WIP: test case for ResourceClaim cache across update"
    This reverts commit 33baa096868ff1644dec826cc0e59a62afa75513.

--- test/e2e/dra/utils/deploy.go
+++ test/e2e/dra/utils/deploy.go
@@ -81,9 +81,8 @@ import (
 )
 
 type Nodes struct {
-	NodeNames  []string
-	tempDir    string
-	claimStore cache.Store
+	NodeNames []string
+	tempDir   string
 }
 
 // NewNodes selects nodes to run the test on.
@@ -163,7 +162,6 @@ func (nodes *Nodes) init(ctx context.Context, f *framework.Framework, minNodes, 
 		0,
 		nil,
 	)
-	nodes.claimStore = claimInformer.GetStore()
 	cancelCtx, cancel := context.WithCancelCause(context.Background())
 	var wg sync.WaitGroup
 	ginkgo.DeferCleanup(func() {
@@ -199,15 +197,6 @@ func (nodes *Nodes) init(ctx context.Context, f *framework.Framework, minNodes, 
 		defer wg.Done()
 		claimInformer.Run(cancelCtx.Done())
 	}()
-}
-
-func (nodes *Nodes) ListCachedResourceClaims() []*resourceapi.ResourceClaim {
-	objs := nodes.claimStore.List()
-	claims := make([]*resourceapi.ResourceClaim, len(objs))
-	for i := range objs {
-		claims[i] = objs[i].(*resourceapi.ResourceClaim)
-	}
-	return claims
 }
 
 type watchWrapper struct {
